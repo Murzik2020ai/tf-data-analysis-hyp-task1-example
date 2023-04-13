0b212aa01b5bfac6ed0dacc58be20d7481bae5ec
Murzik2020ai <61930598+Murzik2020ai@users.noreply.github.com>
2023-04-13 19:01:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,36 +4,23 @@ import numpy as np
 
 chat_id = 1226526788
 
-from scipy.stats import norm
+import statsmodels.api as sm
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int,
-            level=0.03) -> bool:
-    '''
-    Мы тестируем новый способ продажи и ожидаем повышения конверсии в тестовой группе.
-    Составьте критерий с уровнем значимости 0.03 который позволит по данным теста 
-    принять решение о масштабировании теста.
-    ----
-    x_success: int
-        количество покупок в первой выборке
-    x_cnt: int
-        количество заявок в первой выборке
-    y_success: int
-        количество покупок во второй выборке
-    y_cnt: int
-        количество заявок во второй выборке
-    Returns
-    ----
-    bool
-        Возвращаемое значение: bool-значение, ответ на вопрос: "Отклонить гипотезу"
-    '''
-    p1 = x_success/x_cnt
-    p2 = y_success/y_cnt
-    # оценим вероятность успеха на выборке А + Б
-    p = (x_success + y_success)/(x_cnt +y_cnt)
-    # расчитаем z-метку, которая распределена нормально
-    z = (p2 - p1)/ ((p*(1-p)*((1/x_cnt) + (1/y_cnt)))**0.5)    
-    p_value = norm.cdf(z)
-    return True if 2 * p_value < level else False
+
+def solution( s1, n1 ,s2, n2):
+    z, p_value = sm.stats.proportions_ztest([s1,s2],[n1,n2])
+    return True if p_value < 0.03 else False
+
+#def solution(x_success: int, 
+#             x_cnt: int, 
+#             y_success: int, 
+#             y_cnt: int,
+#            level=0.03) -> bool:
+#    p1 = x_success/x_cnt
+#    p2 = y_success/y_cnt
+#    # оценим вероятность успеха на выборке А + Б
+#    p = (x_success + y_success)/(x_cnt +y_cnt)
+#    # расчитаем z-метку, которая распределена нормально
+#    z = (p2 - p1)/ ((p*(1-p)*((1/x_cnt) + (1/y_cnt)))**0.5)    
+#    p_value = norm.cdf(z)
+#    return True if 2 * p_value < level else False
